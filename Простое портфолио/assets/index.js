6ce731612af0e8865ac10dda51071eff3192ecc5
akhmetianov.ea <akhmetianov.ea@parma.ru>
2023-02-13 19:16:12 +0500

    portfolio

---
+++ Простое портфолио/assets/index.js
@@ -0,0 +1,34 @@
+document.querySelector(".home-button").onclick = function(){
+    document.querySelector(".header").classList.add("menu-active");
+    console.log("buttonclick");
+  }
+
+document.querySelector("#link-home").onclick = function(){
+    document.querySelector(".header").classList.remove("menu-active");
+    console.log("buttonclick");
+  }
+
+document.querySelector("#link-img").onclick = function(){
+    document.querySelector(".header").classList.remove("menu-active");
+    console.log("buttonclick");
+  }
+
+document.querySelector("#link-about").onclick = function(){
+    document.querySelector(".header").classList.remove("menu-active");
+    console.log("buttonclick");
+  }
+
+document.querySelector("#link-skills").onclick = function(){
+    document.querySelector(".header").classList.remove("menu-active");
+    console.log("buttonclick");
+  }
+
+document.querySelector("#link-portfolio").onclick = function(){
+    document.querySelector(".header").classList.remove("menu-active");
+    console.log("buttonclick");
+  }
+
+document.querySelector("#link-contacts").onclick = function(){
+    document.querySelector(".header").classList.remove("menu-active");
+    console.log("buttonclick");
+  }
---
+++ тестовое/comebacker/script.js
@@ -0,0 +1,27 @@
+$(function() {
+	$('.close-over, .close-undermodal').on('click', function () {
+		$('#comebacker').fadeOut(300); // 300 скорость исчезновения | disappear speed
+		$('body').removeClass('comebackerhidden');
+	});
+
+	var comebacker = true;
+	
+	$(window).mouseout(function(e){
+		if(e.pageY - $(window).scrollTop() < 1 && comebacker == true){
+			$('#comebacker').fadeIn(300); 
+			$('body').addClass('comebackerhidden');
+			comebacker = false;
+		}
+	});
+
+	try {
+		setTimeout(
+			function show_comebacker() {
+				$('#comebacker').fadeIn(300); // 300 скорость появления | appear speed 
+				$('body').addClass('comebackerhidden');
+				console.log("HELLO");
+			}, 300 //Время появления в милисекундах | Appear time in milliseconds
+		)
+	}
+	catch (e) {}
+});
